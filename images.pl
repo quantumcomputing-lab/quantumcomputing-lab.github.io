10336c0fbcea7c3ea9bcfa068c709868896137e2
Dasgupta, Samudra <dasguptas@ornl.gov>
2023-11-25 11:14:16 +0530

    Initial commit

---
+++ images.pl
@@ -0,0 +1,26 @@
+# LaTeX2HTML 2022 (Released January 1, 2022)
+# Associate images original text with physical files.
+
+
+$key = q/displaystyle;Biglvert{y}Bigrangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 4.40ex; vertical-align: -1.63ex; " SRC="|."$dir".q|img4.svg"
+ ALT="$\displaystyle \; \Big\lvert {y} \Big\rangle$">|; 
+
+$key = q/displaystyle;lvert{y}rangle;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$\displaystyle \; \lvert {y} \rangle$">|; 
+
+$key = q/displaystyleBiglangle{x^{c^k}_{a_b}}Bigrvert;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 4.40ex; vertical-align: -1.63ex; " SRC="|."$dir".q|img3.svg"
+ ALT="$\displaystyle \Big\langle {x^{c^k}_{a_b}} \Big\rvert$">|; 
+
+$key = q/displaystylelangle{x}rvert;MSF=1.6;AAT/;
+$cached_env_img{$key} = q|<IMG
+ STYLE="height: 2.55ex; vertical-align: -0.70ex; " SRC="|."$dir".q|img1.svg"
+ ALT="$\displaystyle \langle {x} \rvert$">|; 
+
+1;
+
